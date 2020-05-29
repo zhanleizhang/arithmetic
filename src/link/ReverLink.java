6bea92e4db600bcd296398e0bbd53d4b9adc9810
张占雷 <zhanlei.zhang@wenba100.com>
2020-05-29 13:57:12 +0800

    递归逆序链表

--- src/link/ReverLink.java
+++ src/link/ReverLink.java
@@ -24,6 +24,14 @@ public class ReverLink {
     /**
      * 递归遍历反转链表
      *
+     * 1 2 3    4 5
+     * 解析：递归遍历
+     * head 等于null 或者head.getNext为空作为函数终止条件
+     * 当它们不等于null时，进行递归操作
+     * reverLinkList1(head.getNext()); 当head.getNext()是最后一个节点5时，直接返回newHead
+     * head.getNext().setNext(head); 把节点5的下一个节点设置成4就是当前head节点，它们两个节点反转
+     * head.setNext(null) 断开head的下一个指向，新旧链表断开，第一次4断开5
+     *
      * @param head
      * @return
      */
